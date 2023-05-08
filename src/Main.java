6ab267e64b0915b9458273b7bd9b00658aa809e5
sudarshankapali <sudarshankapalisk@gmail.com>
2023-05-08 14:53:08 +0545

    Added background music

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.sound.sampled.*;
 public class Main {
     public static void main(String[] args) {
+        String sound="backgroundMusic.wav";
+        BackgroundMusic music =  new BackgroundMusic();
+        music.setFile(sound);
+        music.play();
 //       CarGameGUI game = new CarGameGUI();
         Option frame = new Option();
     }
